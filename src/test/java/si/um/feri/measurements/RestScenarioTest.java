4172a4a169d44550d16b9561af80ae18165ab3c2
Luka P <luka.pavlic@gmail.com>
2024-01-23 22:33:50 +0100

    Working on unit tests

--- src/test/java/si/um/feri/measurements/RestScenarioTest.java
+++ src/test/java/si/um/feri/measurements/RestScenarioTest.java
@@ -1,6 +1,29 @@
 package si.um.feri.measurements;
 
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import si.um.feri.measurements.dao.MeasurementRepository;
+import si.um.feri.measurements.dto.Product;
+import si.um.feri.measurements.rest.MeasurementController;
+import si.um.feri.measurements.rest.MeasurementHistoryController;
+import si.um.feri.measurements.rest.ProductController;
+
+@QuarkusTest
 public class RestScenarioTest {
+
+    @Inject
+    ProductController productController;
+
+    @Inject
+    MeasurementController measurementPostController;
+
+    @Inject
+    MeasurementHistoryController measurementHistoryController;
+
+    @Inject
+    MeasurementRepository measurementRepository;
+
+    private Product newProduct;
 
 
 }
